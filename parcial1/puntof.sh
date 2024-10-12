25a0fb0e49a65a2b697e938caead393dedae9702
Francisco Ezequiel Franco <ezequielfranco91218@gmail.com>
2024-10-12 18:53:15 +0000

    ejercicio F

--- parcial1/puntof.sh
+++ parcial1/puntof.sh
@@ -1,1 +1,6 @@
-vagrant
+curl -s ifconfig.me
+echo "Mi IP pública es: $(curl -s ifconfig.me)" > filtroavanzado.txt
+whoami
+echo "mi Usuario es: $(whoami)" >> filtroavanzado.txt
+echo "El hash o password encriptada de mi usuario es: $(sudo grep $(whoami) /etc/shadow | awk -F ':' '{print $2}')" >> filtroavanzado.txt
+cat filtroavanzado.txtvagrant
