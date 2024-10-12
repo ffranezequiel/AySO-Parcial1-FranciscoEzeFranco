2dcbb5ee98b7fb209d9582adfc8037291fc90aad
Francisco Ezequiel Franco <ezequielfranco91218@gmail.com>
2024-10-12 18:32:03 +0000

    ejercicio E

--- parcial1/puntoe.sh
+++ parcial1/puntoe.sh
@@ -1,1 +1,12 @@
-vagrant
+cat /proc/meminfo
+cat /proc/meminfo |grep MemTotal
+MemTotal:        2010896 kB
+cat /proc/meminfo |grep MemTotal > filtrobasico.txt
+cat filtrobasico.txt
+MemTotal:        2010896 kB
+
+dmidecode -t chassis | grep Manufacturer
+sudo dmidecode -t chassis | grep Manufacturer >> filtrobasico.txt
+cat filtrobasico.txt
+MemTotal:        2010896 kB
+        Manufacturer: Oracle Corporationvagrant
