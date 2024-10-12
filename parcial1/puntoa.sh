6da9b70dbcb3408aa1a3ebe5323f1a926f4410fe
Francisco Ezequiel Franco <ezequielfranco91218@gmail.com>
2024-10-12 05:00:55 +0000

    subiendo modificacion del punto a y ejercicio c

--- parcial1/puntoa.sh
+++ parcial1/puntoa.sh
@@ -1,1 +1,21 @@
-sudo mkdir -p Examenes-UTN/{profesores,alumno_{1..3}/parcial_{1..3}}
+sudo mkdir -p Examenes-UTN/{profesores,alumno_{1..3}/parcial_{1..3}
+
+tree Examenes-UTN/
+
+Examenes-UTN/
+├── alumno_1
+│   ├── parcial_1
+│   ├── parcial_2
+│   └── parcial_3
+├── alumno_2
+│   ├── parcial_1
+│   ├── parcial_2
+│   └── parcial_3
+├── alumno_3
+│   ├── parcial_1
+│   ├── parcial_2
+│   └── parcial_3
+└── profesores
+
+vim puntoa.sh
+cat puntoa.sh}
--- parcial1/puntoc.sh
+++ parcial1/puntoc.sh
@@ -1,1 +1,32 @@
-vagrant
+sudo useradd developer1
+sudo useradd tester1
+sudo useradd devops1
+sudo useradd devops2
+
+sudo groupadd grupodevops
+sudo groupadd grupodeveloper
+sudo groupadd grupotester
+sudo usermod -aG grupodevops,grupodeveloper developer1
+sudo usermod -aG grupotester tester1
+sudo usermod -aG grupodevops,grupodeveloper devops1
+sudo usermod -aG grupodevops devops2
+
+sudo chown developer1:grupodevelopers Examenes-UTN/alumno_1
+sudo chown tester1:grupotesters Examenes-UTN/alumno_2
+sudo chown devops1:grupodevops Examenes-UTN/alumno_3
+sudo chown devops2:grupodevops Examenes-UTN/profesores
+
+sudo chmod 750 /Examenes-UTN/alumno_1
+sudo chmod 760 /Examenes-UTN/alumno_2
+sudo chmod 700 /Examenes-UTN/alumno_3
+sudo chmod 755 /Examenes-UTN/profesores
+
+sudo su developer1 -c "whoami > /Examenes-UTN/alumno_1/validar.txt"
+sudo su tester1 -c "whoami > /Examenes-UTN/alumno_2/validar.txt"
+sudo su devops1 -c "whoami > /Examenes-UTN/alumno_3/validar.txt"
+sudo su devops2 -c "whoami > /Examenes-UTN/profesores/validar.txt"
+
+sudo cat Examenes-UTN/alumno_1/validar.txt
+sudo cat Examenes-UTN/alumno_2/validar.txt
+sudo cat Examenes-UTN/alumno_3/validar.txt
+sudo cat Examenes-UTN/profesores/validar.txtvagrant
